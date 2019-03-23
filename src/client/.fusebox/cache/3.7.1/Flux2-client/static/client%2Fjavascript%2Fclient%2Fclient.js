e30edd2c32d3ea5c2c0cf8b4ebbe35b4d34d6f3d
Kevin Wang <kevinwang5658@gmail.com>
2019-03-22 17:46:25 -0700

    removed .fusebox

--- src/client/.fusebox/cache/3.7.1/Flux2-client/static/client%2Fjavascript%2Fclient%2Fclient.js
+++
@@ -1,9 +0,0 @@
-module.exports = { contents: "'use strict';\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar clientsocketmanager_1 = require(\"./clientsocketmanager\");\nvar io = require(\"socket.io-client\");\nvar client = document.getElementById('download');\nvar progress = document.getElementById('progress');\nvar code = document.getElementById('code');\nvar reveal = document.getElementById('reveal');\nvar slide_down = document.getElementById('text-wrapper');\nvar download_speed = document.getElementById('download-speed');\nvar file_name = document.getElementById('file-name');\nvar file_size = document.getElementById('file-size');\nvar ROOM_ID = code.textContent;\nvar FILE_NAME = file_name.textContent;\nvar FILE_SIZE = Number(file_size.textContent);\nvar socket = io.connect();\nvar socketManager = new clientsocketmanager_1.ClientSocketManager(socket, ROOM_ID);\n//******************************\n// Document events\n//******************************\nclient.addEventListener('click', function () {\n    client.disabled = true;\n    client.style.background = \"#62A4F0\";\n    reveal.style.transform = 'translate(0px, 60px';\n    slide_down.style.transform = 'translate(0px, 200px)';\n    setTimeout(function () {\n        reveal.style.visibility = 'visible';\n    }, 750);\n    socketManager.requestDownload();\n});\nsocketManager.onprogresschanged = function (num) {\n    progress.innerText = num.toFixed(2) + \"%\";\n};\n",
-dependencies: ["./clientsocketmanager","socket.io-client"],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1553223799160,
-devLibsRequired : undefined,
-ac : undefined,
-_ : {}
-}
--- src/client/.fusebox/cache/3.7.1/Flux2-host/static/client%2Fjavascript%2Fconnection%2Fexternalpromise.js
+++
@@ -1,9 +0,0 @@
-module.exports = { contents: "(function (factory) {\n    if (typeof module === \"object\" && typeof module.exports === \"object\") {\n        var v = factory(require, exports);\n        if (v !== undefined) module.exports = v;\n    }\n    else if (typeof define === \"function\" && define.amd) {\n        define([\"require\", \"exports\"], factory);\n    }\n})(function (require, exports) {\n    \"use strict\";\n    Object.defineProperty(exports, \"__esModule\", { value: true });\n    var ExternalPromise = /** @class */ (function () {\n        function ExternalPromise() {\n            var _this = this;\n            this.promise = new Promise(function (resolve, reject) {\n                _this.resolve = resolve;\n                _this.reject = reject;\n            });\n        }\n        return ExternalPromise;\n    }());\n    exports.ExternalPromise = ExternalPromise;\n});\n//# sourceMappingURL=externalpromise.js.map",
-dependencies: [],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1553221562131,
-devLibsRequired : undefined,
-ac : undefined,
-_ : {}
-}
