342c330d14cc7874197a25cd908b2787a8e4dca2
Kevin Wang <kevinwang5658@gmail.com>
2019-04-10 22:51:03 -0700

    server tests

---
+++ tests/server/rtc/base.ts
@@ -0,0 +1,32 @@
+import {Base} from "../../../src/server/rtc/base";
+import {beforeEach, describe} from "selenium-webdriver/testing";
+import {Message, MessageAction, MessageType} from "../../../src/client/javascript/connection/message";
+import {Socket} from "socket.io";
+import * as TypeMoq from "typemoq";
+
+describe('Base', () => {
+    let socketMock: TypeMoq.IMock<Socket>;
+    let instance: Base;
+
+    const ID = "id1";
+
+    beforeEach(() => {
+        socketMock = TypeMoq.Mock.ofType<Socket>();
+        socketMock.setup(x => x.to(TypeMoq.It.isAnyString())).returns(() => socketMock.object);
+        socketMock.setup(x => x.id).returns(() => ID);
+
+        instance = new Base(socketMock.object);
+    });
+
+    it('should relay messages to others in room', async () => {
+        let message = new Message(ID, MessageType.Data, MessageAction.Answer, "content");
+        let rooms = {ID: "socket1", "id2": "socket2"};
+        socketMock.setup(x => x.rooms).returns(() => rooms);
+
+        instance.onmessage(message);
+        socketMock.verify(x => x.to("id2"), TypeMoq.Times.once());
+        socketMock.verify(x => x.to("id1"), TypeMoq.Times.never());
+
+    })
+
+});
