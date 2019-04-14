85991b3c1d4f1a35152ea960c6ba91ce2efefa95
Kevin Wang <kevinwang5658@gmail.com>
2019-04-14 11:51:34 -0700

    hopefully 7

--- tasks/test.js
+++ tasks/test.js
@@ -32,7 +32,7 @@ gulp.task('run-end2end', done => {
 });
 
 gulp.task('start-server', done => {
-    exec('cd ../ && pm2 start dist/server/server.js', (err, stdout, stderr) => {
+    exec('pm2 start dist/server/server.js', (err, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         done(err);
