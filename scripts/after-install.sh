6434a85a51fc38aa6c5912f3e3583c0e7c7c00ed
Kevin Wang <kevinwang5658@gmail.com>
2019-04-14 21:27:35 -0700

    added nvm

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -4,6 +4,9 @@ exec &> /var/log/aws/codedeploy-agent/codedeploy-agent.log
 
 cd /home/ubuntu/kobetransfer
 
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
 npm run clean
 npm run install
 npm run build
--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,4 +2,7 @@
 
 exec &> /var/log/aws/codedeploy-agent/codedeploy-agent.log
 
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
 pm2 start -f dist/server/server.js
