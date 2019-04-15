f2d85e247aeeabe56fbfad3de2c3470b0686b6b3
Kevin Wang <kevinwang5658@gmail.com>
2019-04-15 00:15:53 -0700

    hopefully20

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-exec &> /var/log/aws/codedeploy-agent/codedeploy-agent.log
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>~/log-ainstall.out 2>&1
 
 cd /home/ubuntu/kobetransfer
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,10 @@ exec &> /var/log/aws/codedeploy-agent/codedeploy-agent.log
 
 source /home/ubuntu/.bashrc
 
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>~/log-install.out 2>&1
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
