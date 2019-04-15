e1469e01fd948f3dda4df09a290ed4c7f155707e
Kevin Wang <kevinwang5658@gmail.com>
2019-04-15 00:45:30 -0700

    hopefully21-1

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#exec 3>&1 4>&2
-#trap 'exec 2>&4 1>&3' 0 1 2 3
-#exec 1>/home/ubuntu/log-ainstall.out 2>&1
-#
-#cd /home/ubuntu/kobetransfer
-#
-#chown -R ubuntu:ubuntu .
-#
-#su -c "./install.sh" ubuntu
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/home/ubuntu/log-ainstall.out 2>&1
+
+cd /home/ubuntu/kobetransfer
+
+chown -R ubuntu:ubuntu .
+
+su -c "./install.sh" ubuntu
