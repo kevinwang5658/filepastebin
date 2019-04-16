e0e6da33bb666e1b10bcad343ac8582f7ce4533d
Kevin Wang <kevinwang5658@gmail.com>
2019-04-15 20:41:34 -0700

    new install scripts

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ubuntu/log-ainstall.out 2>&1
+exec &>> /home/ubuntu/install-logs/log-ainstall.out
 
 set -e
 
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ubuntu/log-binstall.out 2>&1
+exec &>> /home/ubuntu/install-logs/log-binstall.out
+
+set -e
 
 rm -rf /home/ubuntu/kobetransfer/{,.[!.],..?}*
