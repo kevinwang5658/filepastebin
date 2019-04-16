8eae59df2f696920b651c841dc46af841ea238b3
Kevin Wang <kevinwang5658@gmail.com>
2019-04-15 20:38:19 -0700

    new install scripts

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>/home/ubuntu/log-astart.out 2>&1
+exec &>> /home/ubuntu/install-logs/log-astart.out
+
+set -e
 
 cd /home/ubuntu/kobetransfer
 
