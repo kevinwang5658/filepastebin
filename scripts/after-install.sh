63bd8412f85995a81f572d453b77c0bc884da9d0
Kevin Wang <kevinwang5658@gmail.com>
2021-02-19 12:49:34 -0500

    [CLEAN-UP] Clean up project and update dependencies

--- scripts/after-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-exec &>> /home/ubuntu/install-logs/log-ainstall.out
-
-set -e
-
-cd /home/ubuntu/kobetransfer
-chown -R ubuntu:ubuntu /home/ubuntu/kobetransfer
--- scripts/before-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-exec &>> /home/ubuntu/install-logs/log-binstall.out
-
-set -e
-
-rm -rf /home/ubuntu/kobetransfer/{,.[!.],..?}*
