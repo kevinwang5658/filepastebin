51682bad8d67ad4e216274ef2ab9a9d8fe68f5fb
Kevin Wang <kevinwang5658@gmail.com>
2021-03-01 22:00:17 -0500

    Updated logo and meta description

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-export docker=/usr/bin/docker
-
-docker context use default
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/t9u2b2i6
+docker login
 docker build -t filepastebin .
-docker tag filepastebin:latest public.ecr.aws/t9u2b2i6/filepastebin:latest
-docker push public.ecr.aws/t9u2b2i6/filepastebin:latest
+docker tag filepastebin:latest kevinwang5658/filepastebin
+docker push kevinwang5658/filepastebin
 
-docker context use myecscontext
-docker compose up
+aws lightsail create-container-service-deployment --cli-input-yaml file://lightsail-deployment.yaml
+
