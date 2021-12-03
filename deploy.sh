d44ba471d867837e0191aa86bda42b0725ec4fed
kevinwang <kevinwang5658@gmail.com>
2021-12-03 10:12:15 -0500

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -p|--profile)
+      PROFILE="$2"
+      shift
+      shift
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+  esac
+done
+
+[ -z "$PROFILE" ] && PROFILE=default
+
 docker login
 docker build -t filepastebin .
 docker tag filepastebin:latest kevinwang5658/filepastebin
 docker push kevinwang5658/filepastebin
 
-aws lightsail create-container-service-deployment --cli-input-yaml file://lightsail-deployment.yaml
+AWS_PROFILE=$PROFILE aws lightsail create-container-service-deployment --cli-input-yaml file://lightsail-deployment.yaml
 
